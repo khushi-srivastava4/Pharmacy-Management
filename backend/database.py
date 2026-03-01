cba5dad9e77edc8059ecb8f73d9f0c4e35f707e2
Khushi Srivastava <khushi.srivastav246@gmail.com>
2026-03-01 14:06:18 +0530

    fix: render sqlite path

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/pharmacy.db")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:////tmp/pharmacy.db"
+)
 engine= create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
